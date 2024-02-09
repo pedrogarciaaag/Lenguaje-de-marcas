c28c73f9c795d2f1566a85e44106da1f94ecb781
pedrogarciaaag <pedrogarciaaag@gmail.com>
2024-02-09 20:22:02 +0100

    Index

--- myapp/src/ToDoItem.js
+++ myapp/src/ToDoItem.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
 function ToDoItem({content, removeTask}){
     return (
         <li>
             {content}
-            <button onClick={removeTask}>X</button>
+            <Button variant="danger" onClick={removeTask}>X</Button>
         </li>
     )
 }
-export default ToDoItem
+export default ToDoItem;
