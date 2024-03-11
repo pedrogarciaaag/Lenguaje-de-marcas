6eaf44d658e21a3ab000b27e7fffc5142f16c133
pedrogarciaaag <pedrogarciaaag@gmail.com>
2024-03-11 19:13:41 +0100

    build

--- actividad_33/src/components/Nav.tsx
+++ actividad_33/src/components/Nav.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -15,11 +15,11 @@ function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href = "/">Inicio</Nav.Link>
-            <Nav.Link href = "/intereses">Puntos de interes</Nav.Link>
-            <Nav.Link href = "/restaurants">Restaurantes</Nav.Link>
-            <Nav.Link href = "/hotels">Hoteles</Nav.Link>
-            <Nav.Link href = "/activitats">Activitades</Nav.Link>
+            <Nav.Link as={NavLink} to = "">Inicio</Nav.Link>
+            <Nav.Link as={NavLink} to = "intereses">Puntos de interes</Nav.Link>
+            <Nav.Link as={NavLink} to = "restaurants">Restaurantes</Nav.Link>
+            <Nav.Link as={NavLink} to = "hotels">Hoteles</Nav.Link>
+            <Nav.Link as={NavLink} to = "activitats">Activitades</Nav.Link>
             
           </Nav>
         </Navbar.Collapse>
